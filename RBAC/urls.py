637e2ebd8dca4998b21710727f7a585d388087ea
Dada Oluwatimileyin <oluwatimileyin962@gmail.com>
2025-04-11 20:02:16 +0100

    Added root urls file

--- RBAC/urls.py
+++ RBAC/urls.py
@@ -6,7 +6,6 @@ from django.contrib import admin
 from django.urls import path
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh')
 ]
